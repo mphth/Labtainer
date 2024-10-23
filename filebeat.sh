ad431a8ce6d91dab9d6f8cabdae4bcdd0fdddb09
mphth <HieuMPT.B20AT058@stu.ptit.edu.vn>
2024-10-23 09:12:29 +0700

    Create filebeat.sh

---
+++ filebeat.sh
@@ -0,0 +1,23 @@
+|#!/bin/bash
+
+# Cập nhật hệ thống
+sudo apt update && sudo apt upgrade -y
+
+# Cài đặt Java
+sudo apt install openjdk-11-jdk -y
+
+# Cài đặt Elasticsearch
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+sudo apt update
+
+# Cài đặt Nginx (nếu chưa có)
+sudo apt install nginx -y
+
+# Cài đặt Filebeat
+sudo apt install filebeat -y
+
+# Khởi động Filebeat
+sudo systemctl start filebeat
+sudo systemctl enable filebeat
+
